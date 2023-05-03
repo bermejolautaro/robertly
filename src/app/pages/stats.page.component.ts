985101a2440264f82004227d7986976878d3bd83
bermejolautaro <tarolau97@hotmail.com>
2023-05-03 20:39:53 -0300

    Migrated component to signals

--- src/app/pages/stats.page.component.ts
+++ src/app/pages/stats.page.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
 import { Observable, map } from 'rxjs';
@@ -9,20 +9,29 @@ import { ExcerciseLogApiService } from '@services/excercise-log-api.service';
 
 import { SeriesPerMuscleGroupWeeklyComponent } from '@components/series-per-muscle-group-weekly.component';
 import { ExcerciseRow } from '@models/excercise-row.model';
-import { IfNullEmptyArrayPipe } from '@pipes/if-null-empty-array.pipe';
 import { SeriesPerMuscleGroupMonthlyComponent } from '@components/series-per-muscle-group-monthly.component';
 
 @Component({
   selector: 'app-stats-page',
   template: `
-    <div class="container my-4">
-      <app-series-per-muscle-group-weekly class="mb-4" [rows]="rows$ | async | ifNullEmptyArray"></app-series-per-muscle-group-weekly>
-      <app-series-per-muscle-group-monthly class="mb-4" [rows]="rows$ | async | ifNullEmptyArray"></app-series-per-muscle-group-monthly>
+    <div class="min-vh-100 d-flex flex-column justify-content-center">
+      <div class="container my-4" *ngIf="rows$ | async as rows; else loadingSpinner">
+        <app-series-per-muscle-group-weekly class="mb-4" [rows]="rows"></app-series-per-muscle-group-weekly>
+        <app-series-per-muscle-group-monthly class="mb-4" [rows]="rows"></app-series-per-muscle-group-monthly>
+      </div>
+
+      <ng-template #loadingSpinner>
+        <div class="d-flex justify-content-center align-items-center p-3 align-self-center">
+          <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </ng-template>
     </div>
   `,
   styles: [``],
   standalone: true,
-  imports: [AsyncPipe, IfNullEmptyArrayPipe, SeriesPerMuscleGroupWeeklyComponent, SeriesPerMuscleGroupMonthlyComponent],
+  imports: [NgIf, AsyncPipe, SeriesPerMuscleGroupWeeklyComponent, SeriesPerMuscleGroupMonthlyComponent],
 })
 export class StatsPageComponent {
   private readonly excerciseLogApiService = inject(ExcerciseLogApiService);
