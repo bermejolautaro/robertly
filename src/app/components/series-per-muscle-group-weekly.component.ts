e15d6d7c3f61611972cdf94719f3e6086441cc1e
bermejolautaro <tarolau97@hotmail.com>
2023-05-03 20:39:53 -0300

    Migrated component to signals

--- src/app/components/series-per-muscle-group-weekly.component.ts
+++ src/app/components/series-per-muscle-group-weekly.component.ts
@@ -1,13 +1,13 @@
-import ***REMOVED*** AsyncPipe, KeyValuePipe, NgFor, NgIf, TitleCasePipe ***REMOVED*** from '@angular/common';
-import ***REMOVED*** Component, Input ***REMOVED*** from '@angular/core';
+import ***REMOVED*** KeyValuePipe, NgFor, NgIf, TitleCasePipe ***REMOVED*** from '@angular/common';
+import ***REMOVED*** ChangeDetectionStrategy, Component, Input, computed, signal ***REMOVED*** from '@angular/core';
 
 import * as R from 'remeda';
 
+import ***REMOVED*** NgbDropdownModule ***REMOVED*** from '@ng-bootstrap/ng-bootstrap';
+
 import ***REMOVED*** getSeriesAmountPerMuscleGroupWeekly, groupByWeek ***REMOVED*** from '@helpers/excercise-log.helper';
 import ***REMOVED*** ExcerciseRow ***REMOVED*** from '@models/excercise-row.model';
-import ***REMOVED*** BehaviorSubject, Observable, map ***REMOVED*** from 'rxjs';
 import ***REMOVED*** MUSCLE_GROUPS ***REMOVED*** from '@models/constants';
-import ***REMOVED*** NgbDropdownModule ***REMOVED*** from '@ng-bootstrap/ng-bootstrap';
 
 @Component(***REMOVED***
   selector: 'app-series-per-muscle-group-weekly',
@@ -19,38 +19,33 @@ import ***REMOVED*** NgbDropdownModule ***REMOVED*** from '@ng-bootstrap/ng-boot
         </div>
         <div class="mb-3">
           <div ngbDropdown class="d-flex justify-content-center">
-            <button type="button" class="btn btn-outline-primary w-100" ngbDropdownToggle>***REMOVED******REMOVED*** selectedWeek$ | async ***REMOVED******REMOVED***</button>
+            <button type="button" class="btn btn-outline-primary w-100" ngbDropdownToggle>***REMOVED******REMOVED*** selectedWeekDropdownValue() ***REMOVED******REMOVED***</button>
             <div ngbDropdownMenu class="w-100">
-              <button ngbDropdownItem (click)="selectedWeekSubject.next(null)">Clear filter</button>
-              <button ngbDropdownItem *ngFor="let week of weeks" (click)="selectedWeekSubject.next(week)">***REMOVED******REMOVED*** week ***REMOVED******REMOVED***</button>
+              <button ngbDropdownItem *ngFor="let week of weeksSignal()" (click)="selectedWeekDropdownSignal.set(week)">***REMOVED******REMOVED*** week ***REMOVED******REMOVED***</button>
             </div>
           </div>
         </div>
-        <ng-container *ngIf="selectedWeekSubject | async as selectedWeek">
-          <ng-container *ngIf="seriesPerMuscleGroupWeekly && daysGroupByWeek">
-            <table class="table table-sm m-0 mb-3">
-              <thead>
-                <tr>
-                  <td scope="col" class="fw-semibold">Muscle Group</td>
-                  <td class="text-center fw-semibold" *ngFor="let name of seriesPerMuscleGroupWeekly[selectedWeek] | keyvalue">
-                    ***REMOVED******REMOVED*** name.key | titlecase ***REMOVED******REMOVED***
-                  </td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr *ngFor="let muscleGroup of muscleGroups">
-                  <td class="fw-semibold">***REMOVED******REMOVED*** muscleGroup | titlecase ***REMOVED******REMOVED***</td>
-                  <td class="text-center" *ngFor="let x of seriesPerMuscleGroupWeekly[selectedWeek] | keyvalue">
-                    ***REMOVED******REMOVED*** x.value[muscleGroup] || 0 ***REMOVED******REMOVED***
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <div class="fw-semibold">
-              ***REMOVED******REMOVED*** daysGroupByWeek[selectedWeek] ***REMOVED******REMOVED*** ***REMOVED******REMOVED*** daysGroupByWeek[selectedWeek] === 1 ? 'day' : 'days' ***REMOVED******REMOVED*** trained this week
-            </div>
-          </ng-container>
-        </ng-container>
+        <table class="table table-sm m-0 mb-3">
+          <thead>
+            <tr>
+              <td scope="col" class="fw-semibold">Muscle Group</td>
+              <td class="text-center fw-semibold" *ngFor="let name of seriesPerMuscleGroupWeeklySignal()[selectedWeekSignal()] | keyvalue">
+                ***REMOVED******REMOVED*** name.key | titlecase ***REMOVED******REMOVED***
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr *ngFor="let muscleGroup of muscleGroups">
+              <td class="fw-semibold">***REMOVED******REMOVED*** muscleGroup | titlecase ***REMOVED******REMOVED***</td>
+              <td class="text-center" *ngFor="let x of seriesPerMuscleGroupWeeklySignal()[selectedWeekSignal()] | keyvalue">
+                ***REMOVED******REMOVED*** x.value[muscleGroup] || 0 ***REMOVED******REMOVED***
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="fw-semibold">
+          ***REMOVED******REMOVED*** daysTrainedMessage() ***REMOVED******REMOVED***
+        </div>
       </div>
     </div>
   `,
@@ -62,30 +57,26 @@ import ***REMOVED*** NgbDropdownModule ***REMOVED*** from '@ng-bootstrap/ng-boot
     `,
   ],
   standalone: true,
-  imports: [NgFor, NgIf, TitleCasePipe, KeyValuePipe, AsyncPipe, NgbDropdownModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgFor, NgIf, TitleCasePipe, KeyValuePipe, NgbDropdownModule],
 ***REMOVED***)
 export class SeriesPerMuscleGroupWeeklyComponent ***REMOVED***
-  private _rows: ExcerciseRow[] = [];
-
   @Input() public set rows(value: ExcerciseRow[]) ***REMOVED***
-    this._rows = value;
-    this.seriesPerMuscleGroupWeekly = getSeriesAmountPerMuscleGroupWeekly(this.rows);
-    this.daysGroupByWeek = R.mapValues(groupByWeek(this.rows), x => x.length);
-
-    this.weeks = R.keys(this.seriesPerMuscleGroupWeekly);
-    this.selectedWeekSubject.next(this.weeks[0]);
-***REMOVED***
-
-  public get rows(): ExcerciseRow[] ***REMOVED***
-    return this._rows;
+    this.rowsSignal.set(value);
 ***REMOVED***
 
   public readonly muscleGroups = MUSCLE_GROUPS;
 
-  public seriesPerMuscleGroupWeekly: Record<string, Record<string, Record<string, number>>> | null = null;
-  public daysGroupByWeek: Record<PropertyKey, number> | null = null;
-  public weeks: string[] = [];
+  public readonly rowsSignal = signal<ExcerciseRow[]>([]);
+  public readonly daysGroupByWeekSignal = computed(() => R.mapValues(groupByWeek(this.rowsSignal()), x => x.length));
+  public readonly seriesPerMuscleGroupWeeklySignal = computed(() => getSeriesAmountPerMuscleGroupWeekly(this.rowsSignal()));
+  public readonly weeksSignal = computed(() => R.keys(this.seriesPerMuscleGroupWeeklySignal()));
+  public readonly selectedWeekDropdownSignal = signal<string | null>(null);
+  public readonly selectedWeekSignal = computed(() => this.selectedWeekDropdownSignal() ?? this.weeksSignal()[0]);
+  public readonly selectedWeekDropdownValue = computed(() => this.selectedWeekSignal() ?? 'Week');
 
-  public selectedWeekSubject: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
-  public selectedWeek$: Observable<string> = this.selectedWeekSubject.pipe(map(x => x ?? 'Week'));
+  public readonly daysTrainedMessage = computed(() => ***REMOVED***
+    const daysTrained = this.daysGroupByWeekSignal()[this.selectedWeekSignal()];
+    return `$***REMOVED***daysTrained***REMOVED*** $***REMOVED***daysTrained === 1 ? 'day' : 'days'***REMOVED*** trained this week`;
+***REMOVED***);
 ***REMOVED***
--- src/app/pages/stats.page.component.ts
+++ src/app/pages/stats.page.component.ts
@@ -1,4 +1,4 @@
-import ***REMOVED*** AsyncPipe ***REMOVED*** from '@angular/common';
+import ***REMOVED*** AsyncPipe, NgIf ***REMOVED*** from '@angular/common';
 import ***REMOVED*** Component, inject ***REMOVED*** from '@angular/core';
 
 import ***REMOVED*** Observable, map ***REMOVED*** from 'rxjs';
@@ -9,20 +9,29 @@ import ***REMOVED*** ExcerciseLogApiService ***REMOVED*** from '@services/excerc
 
 import ***REMOVED*** SeriesPerMuscleGroupWeeklyComponent ***REMOVED*** from '@components/series-per-muscle-group-weekly.component';
 import ***REMOVED*** ExcerciseRow ***REMOVED*** from '@models/excercise-row.model';
-import ***REMOVED*** IfNullEmptyArrayPipe ***REMOVED*** from '@pipes/if-null-empty-array.pipe';
 import ***REMOVED*** SeriesPerMuscleGroupMonthlyComponent ***REMOVED*** from '@components/series-per-muscle-group-monthly.component';
 
 @Component(***REMOVED***
   selector: 'app-stats-page',
   template: `
-    <div class="container my-4">
-      <app-series-per-muscle-group-weekly class="mb-4" [rows]="rows$ | async | ifNullEmptyArray"></app-series-per-muscle-group-weekly>
-      <app-series-per-muscle-group-monthly class="mb-4" [rows]="rows$ | async | ifNullEmptyArray"></app-series-per-muscle-group-monthly>
+    <div class="min-vh-100 d-flex flex-column justify-content-center">
+      <div class="container my-4" *ngIf="rows$ | async as rows; else loadingSpinner">
+        <app-series-per-muscle-group-weekly class="mb-4" [rows]="rows"></app-series-per-muscle-group-weekly>
+        <app-series-per-muscle-group-monthly class="mb-4" [rows]="rows"></app-series-per-muscle-group-monthly>
+      </div>
+
+      <ng-template #loadingSpinner>
+        <div class="d-flex justify-content-center align-items-center p-3 align-self-center">
+          <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </ng-template>
     </div>
   `,
   styles: [``],
   standalone: true,
-  imports: [AsyncPipe, IfNullEmptyArrayPipe, SeriesPerMuscleGroupWeeklyComponent, SeriesPerMuscleGroupMonthlyComponent],
+  imports: [NgIf, AsyncPipe, SeriesPerMuscleGroupWeeklyComponent, SeriesPerMuscleGroupMonthlyComponent],
 ***REMOVED***)
 export class StatsPageComponent ***REMOVED***
   private readonly excerciseLogApiService = inject(ExcerciseLogApiService);
