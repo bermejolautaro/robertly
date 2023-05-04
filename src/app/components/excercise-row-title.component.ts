729335e37c6bd8a665c5d6e2b88bc9cccf122d26
bermejolautaro <tarolau97@hotmail.com>
2023-05-04 12:49:29 -0300

    Changed look and feel to be slimmer

--- src/app/components/excercise-row-title.component.ts
+++ src/app/components/excercise-row-title.component.ts
@@ -1,19 +1,20 @@
-import { NgFor, NgIf, TitleCasePipe } from '@angular/common';
+import { NgClass, NgFor, NgIf, TitleCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ExcerciseRow } from '@models/excercise-row.model';
 
 @Component({
   selector: 'app-excercise-row-title',
   template: `
-    <div class="row w-100" *ngIf="excerciseRow">
-      <div *ngIf="showExcercise" class="col d-flex align-items-center justify-content-center text-center">
-        {{ excerciseRow.excerciseName | titlecase }}
+    <div class="w-100">
+      <div class="row w-100 pb-1" [ngClass]="showDate && showUsername ? 'fw-bold': null" *ngIf="excerciseRow">
+        <div *ngIf="showExcercise" class="col d-flex" [style.fontSize.rem]="1">
+          {{ excerciseRow.excerciseName | titlecase }}
+        </div>
       </div>
-      <div *ngIf="showDate" class="col d-flex align-items-center justify-content-center text-center">
-        {{ excerciseRow.date }}
-      </div>
-      <div *ngIf="showUsername" class="col d-flex align-items-center justify-content-center text-center">
-        {{ excerciseRow.username | titlecase }}
+      <div class="row">
+        <div *ngIf="showDate" class="col d-flex text-muted" [style.fontSize.rem]=".8">
+          {{ excerciseRow.date }} - {{ excerciseRow.username | titlecase }}
+        </div>
       </div>
     </div>
   `,
@@ -26,7 +27,7 @@ import { ExcerciseRow } from '@models/excercise-row.model';
     `,
   ],
   standalone: true,
-  imports: [NgFor, NgIf, TitleCasePipe],
+  imports: [NgFor, NgIf, NgClass, TitleCasePipe],
 })
 export class ExcerciseRowTitleComponent {
   @Input() showExcercise: boolean = true;
