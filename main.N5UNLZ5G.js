d3ddd080d9909a0be52d2e603a5ebea5ac4e96af
bermejolautaro <tarolau97@hotmail.com>
2023-07-11 14:57:28 -0300

    Auto-generated commit [ci skip]

--- main.N5UNLZ5G.js
+++
@@ -1,1 +0,0 @@
-import{Ta as a,Ua as b,Va as c}from"./chunk-FSFHDUJR.js";import"./chunk-LWEHNFHP.js";export{c as BACKEND_URL,a as LOGS_PATH,b as STATS_PATH};
---
+++ main.OHJDG7Z3.js
@@ -0,0 +1,1 @@
+import{Ua as a,Va as b,Wa as c}from"./chunk-KV3LPJFH.js";import"./chunk-LWEHNFHP.js";export{c as BACKEND_URL,a as LOGS_PATH,b as STATS_PATH};
