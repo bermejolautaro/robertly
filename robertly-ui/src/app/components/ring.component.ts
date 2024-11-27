4851c84fd4f362a992950d72bdc4ab341d69476b
bermejolautaro <tarolau97@hotmail.com>
2024-11-27 16:30:47 -0300

    Fixed tooltip styles

--- robertly-ui/src/app/components/ring.component.ts
+++ robertly-ui/src/app/components/ring.component.ts
@@ -8,6 +8,7 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
     [attr.height]="this.size()"
     [attr.width]="this.size()"
     ngbTooltip="{{ this.value() }} / {{ this.maxValue() }}"
+    tooltipClass="ring-tooltip"
     ontouchstart
     tabindex="-1"
   >
@@ -45,6 +46,7 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
     svg:focus {
       outline: none;
     }
+
     .progress-ring__text {
       font-size: 24px;
       font-weight: bold;
