1cac0db15788d33a0ff84c05875093d4dc72a5eb
Lautaro Bermejo <tarolau97@hotmail.com>
2024-12-01 19:51:28 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/bermejolautaro/robertly/commit/11ad6884551e043fce450e5164f485f78decd2b1

---
+++ chunk-6SPBBHL6.js
@@ -0,0 +1,1 @@
+import{Lb as n,Mb as d,ec as a,jb as t}from"./chunk-LYCTWYRR.js";var p=(()=>{class e{static{this.\u0275fac=function(o){return new(o||e)}}static{this.\u0275cmp=t({type:e,selectors:[["app-foods-page"]],decls:2,vars:0,template:function(o,i){o&1&&(n(0,"p"),a(1,"foods works!"),d())},encapsulation:2,changeDetection:0})}}return e})();export{p as FoodsPageComponent};
---
+++ chunk-FVSWIPI3.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-6DNQCBZA.js";import"./chunk-C5LQ3MSO.js";import"./chunk-CMFT27PF.js";import"./chunk-LYCTWYRR.js";export{a as RingComponent};
